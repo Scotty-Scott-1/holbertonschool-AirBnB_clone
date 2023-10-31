633f096e2f0f6999b3fdfcc201f32b88ad4944cf
Nadely <6890@holbertonstudents.com>
2023-10-31 15:39:48 +0100

    add args in init in base_model

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,15 @@ class BaseModel:
     """class BaseModel that defines all common attributes/methods
     for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs is None:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+
 
     def __str__(self):
         class_name = self.__class__.__name__
