f8a7c3e31e3392c073a4b4f78a067fe9c78e68be
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-10-31 13:48:29 +0100

    Updated kwargs to set attrs correctly

--- models/base_model.py
+++ models/base_model.py
@@ -11,18 +11,18 @@ class BaseModel:
     for other classes"""
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
         if len(kwargs) < 1:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-
+            self.updated_at = datetime.now()
+        else:
+            self.id = kwargs.get("id", uuid.uuid4())
+            self.created_at = kwargs.get("created_at")
+            self.updated_at = kwargs.get("updated_at")
 
     def __str__(self):
         class_name = self.__class__.__name__
-        return("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
+        return ("[{}] ({}) {}".format(class_name, self.id, self.__dict__))
 
     def save(self):
         """updates the public instance attribute updated_at with the
