1f30ff4c584f5a8783bf2c2b699192e7bbe0dfa9
Nadely <6890@holbertonstudents.com>
2023-11-02 13:00:27 +0100

    correction all class that inherit frome BaseModel

--- models/city.py
+++ models/city.py
@@ -6,7 +6,5 @@ from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.state_id = ""
-        self.name = ""
+    state_id = ""
+    name = ""
--- models/review.py
+++ models/review.py
@@ -5,9 +5,7 @@
 from models.base_model import BaseModel
 
 
-class review(BaseModel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.place_id = ""
-        self.user_id = ""
-        self.text = ""
+class Review(BaseModel):
+    place_id = ""
+    user_id = ""
+    text = ""
