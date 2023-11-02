f3b05b509c116f25156f725fff87fee8b4ca8a70
Nadely <6890@holbertonstudents.com>
2023-11-02 11:53:26 +0100

    uptade user file

--- models/user.py
+++ models/user.py
@@ -2,13 +2,13 @@
 """class User that inherits from BaseModel"""
 
 
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 
 class User(BaseModel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.email = str()
-        self.passord = str()
-        self.firt_name = str()
-        self.last_name = str()
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
