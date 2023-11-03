3f3da1aa910d222b40f3c65462600e39b1974ce9
Nadely <6890@holbertonstudents.com>
2023-11-03 17:07:31 +0100

    another attempt to upgrade the console file

--- console.py
+++ console.py
@@ -17,11 +17,18 @@ from models.amenity import Amenity
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb)"
-    class_list = ["BaseModel", "User", "State", "Review",
-                "Place", "City", "Amenity"]
+    class_list = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Review": Review,
+        "Place": Place,
+        "Amenity": Amenity
+    }
 
     def do_quit(self, arg):
-        """for quit command interpreter"""
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
@@ -33,7 +40,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def help_quit(self):
-        """quit"""
+        """Quit command to exit the program"""
         print("Quit command to exit the program")
         print(' ')
 
@@ -43,30 +50,15 @@ class HBNBCommand(cmd.Cmd):
         print(' ')
 
     def do_create(self, arg):
-        """create an instance of basemodel and print id"""
+        """create an instance of class and print id"""
         args_list = arg.split()
         if len(args_list) == 0:
             print("** class name missing **")
-        elif args_list[0] not in HBNBCommand.class_list:
-            """elif args_list[0] not in HBNBCommand.class_list:"""
+        elif args_list[0] not in self.class_list:
             print("** class doesn't exist **")
         else:
             class_name = args_list[0]
-            if class_name == "BaseModel":
-                obj = BaseModel()
-            elif class_name == "User":
-                obj = User()
-            elif class_name == "State":
-                obj = State()
-            elif class_name == "Review":
-                obj = Review()
-            elif class_name == "Place":
-                obj = Place()
-            elif class_name == "City":
-                obj = City()
-            elif class_name == "Amenity":
-                obj = Amenity()
-
+            obj = self.class_list[class_name]()
             storage.new(obj)
             storage.save()
             print(obj.id)
@@ -79,7 +71,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         else:
             class_name = args_list[0]
-            if class_name not in HBNBCommand.class_list:
+            if class_name not in self.class_list:
                 print("** class doesn't exist **")
 
             elif len(args_list) < 2:
@@ -102,7 +94,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
         else:
             class_name = args_list[0]
-            if class_name not in HBNBCommand.class_list:
+            if class_name not in self.class_list:
                 print("** class doesn't exist **")
 
             elif len(args_list) < 2:
@@ -122,13 +114,13 @@ class HBNBCommand(cmd.Cmd):
         args_list = arg.split()
         if len(args_list) == 0:
             for ob in storage.all().values():
-                my_list.append(ob.__str__())
+                my_list.append(repr(ob))
             print(my_list)
         elif len(args_list) == 1:
-            if args_list[0] in HBNBCommand.class_list:
+            if args_list[0] in self.class_list:
                 for ob in storage.all().values():
-                    if args_list[0] in ob.__str__():
-                        my_list.append(ob.__str__())
+                    if args_list[0] in repr(ob):
+                        my_list.append(repr(ob))
                 print(my_list)
             else:
                 print("** class doesn't exist **")
@@ -139,7 +131,7 @@ class HBNBCommand(cmd.Cmd):
         if len(args_list) == 0:
             print("** class name missing **")
             return
-        elif args_list[0] not in HBNBCommand.class_list:
+        elif args_list[0] not in self.class_list:
             print("** class doesn't exist **")
             return
         elif len(args_list) < 2:
