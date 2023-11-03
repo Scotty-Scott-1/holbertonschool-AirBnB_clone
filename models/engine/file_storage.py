cb7f3fce751f98e3d2d742e9a94f04b9d2536e25
Nadely <6890@holbertonstudents.com>
2023-11-03 17:34:23 +0100

    remove old version for file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,22 +12,19 @@ from models.review import Review
 from models.place import Place
 from models.amenity import Amenity
 
-classes = {
-    'BaseModel': BaseModel,
-    'User': User,
-    'City': City,
-    'State': State,
-    'Review': Review,
-    'Place': Place,
-    'Amenity': Amenity
-}
+
+classes = {'BaseModel': BaseModel, "User": User, "City": City,
+           "State": State, "Review": Review,
+           "Place": Place, "Amenity": Amenity}
 
 
 class FileStorage:
+    """This class manages storage of hbnb models in JSON format"""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
         if cls is None:
             return FileStorage.__objects
         else:
@@ -38,10 +35,12 @@ class FileStorage:
             return new_dict
 
     def new(self, obj):
+        """Adds a new object to storage dictionary"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """Saves storage dictionary to a file"""
         serialized_objects = {}
         for key, obj in FileStorage.__objects.items():
             serialized_objects[key] = obj.to_dict()
@@ -49,11 +48,13 @@ class FileStorage:
             json.dump(serialized_objects, file)
 
     def reload(self):
+        """Loads storage dictionary from a file"""
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, 'r') as file:
                 data = json.load(file)
                 for key, value in data.items():
-                    class_name, obj_id = key.split('.')
+                    class_name = value.get('__class__')
                     if class_name in classes:
-                        obj = classes[class_name](**value)
-                        FileStorage.__objects[key] = obj
+                        self.all()[key] = classes[value['__class__']](**value)
+                    else:
+                        pass
