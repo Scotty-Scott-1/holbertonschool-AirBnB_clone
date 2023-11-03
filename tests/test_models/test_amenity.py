9b9d20ee90a41b1305a07fae8329c7d8a0fd2d0d
Scotty-Scott-1 <swilliamfr@outlook.com>
2023-11-03 13:32:37 +0100

    Amenity tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -4,12 +4,30 @@
 
 import unittest
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models import storage
 
 
 class TestAmenity(unittest.TestCase):
     def test_amenity_attributes(self):
         amenity = Amenity()
         self.assertEqual(amenity.name, "")
+
+    def test_amenity_in_storage(self):
+        amenity = Amenity()
+        storage.save()
+        key = "Amenity.{}".format(amenity.id)
+        self.assertEqual(key in storage.all(), True)
+
+    def test_amenity_set_and_get_name(self):
+        amenity = Amenity()
+        amenity.name = "shop"
+        self.assertEqual(amenity.name, "shop")
+        amenity.name = "park"
+        self.assertEqual(amenity.name, "park")
+
+    def test_amenity_inherits_from(self):
+        self.assertTrue(issubclass(Amenity, BaseModel))
 
 
 if __name__ == "__main__":
